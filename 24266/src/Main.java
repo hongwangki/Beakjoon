ecc357b171abd9e8c6c7302cffc76edf9369ccec
홍왕기 <144398371+hongwangki@users.noreply.github.com>
2024-11-07 11:03:22 +0900

    2024 11/7 beakjoon

---
+++ 24266/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        long n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        System.out.println(n * n * n);
+        System.out.print(3);//(Big O = n^3
+    }
+}
---
+++ 24267/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        long n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        System.out.println((n*(n-1)*(n-2))/6);
+        System.out.print(3);
+    }
+}
